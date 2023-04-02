be0e4d09af31dfe745a6f572285f0ff597d45af8
Aakif Ahmed <aakifahmedd@gmail.com>
2023-04-02 10:48:59 +0530

    Connect main to search screen

--- app/src/main/java/com/example/mobileappcw2/MainActivity.kt
+++ app/src/main/java/com/example/mobileappcw2/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.mobileappcw2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get a reference to the button
+        val button = findViewById<Button>(R.id.button)
+
+        // Set an OnClickListener to the button
+        button.setOnClickListener {
+            // Create an intent to start the SearchActivity
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
